4039c1c9b4c14f4f5ed00f5f3b9282accba8d8ba
Pedro Mafra <pmafrarm@gmail.com>
2021-07-24 03:10:16 -0300

    Template/selecionarOpções/botão

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-function select(element) {
-    element.classList.toggle("item-selected");
+function mainCourse(element) {
+
+    let choice = document.querySelector(".main-course.green-border");
+    if (choice !== null) {
+        choice.classList.toggle("green-border");
+    }
+    
+    element.classList.toggle("green-border");
 }
+
+function drink(element) {
+
+    let choice = document.querySelector(".drinks.green-border");
+    if (choice !== null) {
+        choice.classList.toggle("green-border");
+    }
+    
+    element.classList.toggle("green-border");
+}
+
+function desert(element) {
+
+    let choice = document.querySelector(".deserts.green-border");
+    if (choice !== null) {
+        choice.classList.toggle("green-border");
+    }
+    
+    element.classList.toggle("green-border");
+}
+
+function finish() {
+    let um = document.querySelector(".main-course.green-border");
+    let dois = document.querySelector(".drinks.green-border");
+    let tres = document.querySelector(".deserts.green-border");
+
+    if (um !== null){
+        if(dois !== null){
+            if(tres !== null){
+                let close = document.querySelector(".bottom-bar-content");
+                close.innerHTML = "Fechar pedido";
+                close.classList.add("finish-button");
+            }
+        }
+    }
+}
+
+                
+
+
+
