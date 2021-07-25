affef44666653eef745ae368830a5a3bb1c26866
Pedro Mafra <pmafrarm@gmail.com>
2021-07-24 23:22:13 -0300

    Green Button finished

--- script.js
+++ script.js
@@ -1,63 +1,96 @@
 function mainCourse(element) {
 
     let choice = document.querySelector(".main-course.green-border");
-    let checking = document.querySelector(".main-course .check.checked-icon");
+    let checking = document.querySelector(".main-course .check.green-icon");
     if ((choice && checking) !== null) {
         choice.classList.toggle("green-border");
-        checking.classList.toggle("checked-icon");
+        checking.classList.toggle("green-icon");
     }
     
     element.classList.toggle("green-border");
 
     let checkIcon = document.querySelector(".main-course.green-border .check");
-    checkIcon.classList.toggle("checked-icon");
+    checkIcon.classList.toggle("green-icon");
+
+    let first = document.querySelector(".main-course.green-border");
+    let second = document.querySelector(".drinks.green-border");
+    let third = document.querySelector(".deserts.green-border");
+
+    if (first !== null){
+        if(second !== null){
+            if(third !== null){
+                let close = document.querySelector(".bottom-bar-button");
+                close.innerHTML = "Fechar pedido";
+                close.classList.add("green-button");
+            }
+        }
+    }
+
 }
 
 function drink(element) {
 
     let choice = document.querySelector(".drinks.green-border");
-    let checking = document.querySelector(".drinks .check.checked-icon");
+    let checking = document.querySelector(".drinks .check.green-icon");
     if ((choice && checking) !== null) {
         choice.classList.toggle("green-border");
-        checking.classList.toggle("checked-icon");
+        checking.classList.toggle("green-icon");
     }
     
     element.classList.toggle("green-border");
 
     let checkIcon = document.querySelector(".drinks.green-border .check");
-    checkIcon.classList.toggle("checked-icon");
+    checkIcon.classList.toggle("green-icon");
+
+    let first = document.querySelector(".main-course.green-border");
+    let second = document.querySelector(".drinks.green-border");
+    let third = document.querySelector(".deserts.green-border");
+
+    if (first !== null){
+        if(second !== null){
+            if(third !== null){
+                let close = document.querySelector(".bottom-bar-button");
+                close.innerHTML = "Fechar pedido";
+                close.classList.add("green-button");
+            }
+        }
+    }
+
 }
 
 function desert(element) {
 
     let choice = document.querySelector(".deserts.green-border");
-    let checking = document.querySelector(".deserts .check.checked-icon");
+    let checking = document.querySelector(".deserts .check.green-icon");
     if ((choice && checking) !== null) {
         choice.classList.toggle("green-border");
-        checking.classList.toggle("checked-icon");
+        checking.classList.toggle("green-icon");
     }
     
     element.classList.toggle("green-border");
 
     let checkIcon = document.querySelector(".deserts.green-border .check");
-    checkIcon.classList.toggle("checked-icon");
-}
+    checkIcon.classList.toggle("green-icon");
 
-function finish() {
-    let um = document.querySelector(".main-course.green-border");
-    let dois = document.querySelector(".drinks.green-border");
-    let tres = document.querySelector(".deserts.green-border");
+    let first = document.querySelector(".main-course.green-border");
+    let second = document.querySelector(".drinks.green-border");
+    let third = document.querySelector(".deserts.green-border");
 
-    if (um !== null){
-        if(dois !== null){
-            if(tres !== null){
-                let close = document.querySelector(".bottom-bar-content");
+    if (first !== null){
+        if(second !== null){
+            if(third !== null){
+                let close = document.querySelector(".bottom-bar-button");
                 close.innerHTML = "Fechar pedido";
-                close.classList.add("finish-button");
+                close.classList.add("green-button");
             }
         }
     }
+
 }
+
+
+
+
 
                 
 
