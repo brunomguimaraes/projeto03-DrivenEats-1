5854a20852652750a0d3583de2bf0def2a2a395e
Pedro Mafra <pmafrarm@gmail.com>
2021-07-25 21:25:30 -0300

    confirmation box finished

--- script.js
+++ script.js
@@ -92,18 +92,45 @@ function confirm() {
     let element = document.querySelector(".bottom-bar-button");
     let released = element.innerHTML;
     if (released === "Fechar pedido") {
-        let appearback = document.querySelector(".transparent-background");
-        appearback.classList.add("appearback");
-        let appearbox = document.querySelector(".confirmation-box");
-        appearbox.classList.add("appearbox");
+        let appearBack = document.querySelector(".transparent-background");
+        appearBack.classList.add("appearback");
+        let appearBox = document.querySelector(".confirmation-box");
+        appearBox.classList.add("appearbox");
+
+        let mainDishName = (document.querySelector(".main-course.green-border .menu .name")).innerHTML;
+        let mainDishConfirm = document.querySelector(".confirmation-box .choices .food");
+        mainDishConfirm.innerHTML = mainDishName;
+        let mainDishPrice = (document.querySelector(".main-course.green-border .menu .price")).innerText;
+        let mainDishPriceConfirm = document.querySelector(".confirmation-box .choices .foodPrice");
+        mainDishPriceConfirm.innerHTML = mainDishPrice;
+
+        let drinkName = (document.querySelector(".drinks.green-border .menu .name")).innerHTML;
+        let drinkConfirm = document.querySelector(".confirmation-box .choices .soda");
+        drinkConfirm.innerHTML = drinkName;
+        let drinkPrice = (document.querySelector(".drinks.green-border .menu .price")).innerText;
+        let drinkPriceConfirm = document.querySelector(".confirmation-box .choices .sodaPrice");
+        drinkPriceConfirm.innerHTML = drinkPrice;
+
+        let desertName = (document.querySelector(".deserts.green-border .menu .name")).innerHTML;
+        let desertConfirm = document.querySelector(".confirmation-box .choices .candy");
+        desertConfirm.innerHTML = desertName;
+        let desertPrice = (document.querySelector(".deserts.green-border .menu .price")).innerText;
+        let desertPriceConfirm = document.querySelector(".confirmation-box .choices .candyPrice");
+        desertPriceConfirm.innerHTML = desertPrice;
+
+        let total = (Number(mainDishPrice.replace(',','.').replace('R$ ','')) + Number(drinkPrice.replace(',','.').replace('R$ ','')) + Number(desertPrice.replace(',','.').replace('R$ ','')));
+        let totalPriceConfirm = document.querySelector(".confirmation-box .choices .totalPrice");
+        totalPriceConfirm.innerHTML = total.toFixed(2).replace('.',',');
+        
+
     }
 }
 
 function canceling() {
-    let disappearback = document.querySelector(".transparent-background");
-    disappearback.classList.remove("appearback");
-    let disappearbox = document.querySelector(".confirmation-box");
-    disappearbox.classList.remove("appearbox");
+    let disappearBack = document.querySelector(".transparent-background");
+    disappearBack.classList.remove("appearback");
+    let disappearBox = document.querySelector(".confirmation-box");
+    disappearBox.classList.remove("appearbox");
 }
 
 
